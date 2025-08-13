60dd6d99e5726c93f84c981f9a43ecb0a79b3640
MochaFox <fox@mochafox.com>
2025-08-13 01:10:43 -0500

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
+  console.log(request.nextUrl)
+  console.log(request.url)
   const hostname = request.headers.get('host') || ''
   
   // Handle projects subdomain
@@ -17,15 +19,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - public folder
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$|.*\\.jpg$|.*\\.gif$).*)',
-  ],
+  matcher: '/',
 }
